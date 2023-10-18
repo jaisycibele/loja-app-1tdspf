36a147d884f666862fefab20e5e0a3d8e94c6ded
jaisycibele <jaisycibele@gmail.com>
2023-10-18 11:20:11 -0300

    Criando rotas dinamicas

---
+++ src/app/produtos/(tocas)/[id]/page.jsx
@@ -0,0 +1,10 @@
+export default function ProdutoId({params}){
+    return (
+        <>
+        <div>
+        <h1>Produto ID</h1>
+        <p>Slug: {params.id}</p>
+        </div>
+        </>
+    );
+}
